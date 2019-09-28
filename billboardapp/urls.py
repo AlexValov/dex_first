8db04d6541886c7cdbe119001d3f44616944bb18
AlexValov <alexandrvalov@gmail.com>
2019-09-28 13:00:58 +0300

    correct: urls

--- billboardapp/urls.py
+++ billboardapp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from billboardapp.views import *
 
-app_name = 'product'
 
 urlpatterns = [
-    path('product/create/', ProductCreateView.as_view()),
-    path('product/all/', ProductListView.as_view()),
-    path('product/detail/<int:pk>', ProductDetailView.as_view()),
+    path('products/create/', ProductCreateView.as_view()),
+    path('products/all/', ProductListView.as_view()),
+    path('products/detail/<int:pk>', ProductDetailView.as_view()),
 ]
--- dex/urls.py
+++ dex/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, )
 from rest_framework import routers
 from billboardapp.views import UserViewSet
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from rest_framework_swagger.views import get_swagger_view
-
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
@@ -12,12 +11,12 @@ router.register('users', UserViewSet)
 schema_view = get_swagger_view(title='DEX_test')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', schema_view),
-    path('', include(router.urls)),
-    path('api/billboard/', include('billboardapp.urls')),
-    path('accounts/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('admin/', admin.site.urls),
+    path('api/v1/auth/', include(router.urls)),
+    path('api/v1/accounts/', include('rest_framework.urls')),
+    path('api/v1/billboard/', include('billboardapp.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
